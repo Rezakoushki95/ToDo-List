3ac7fb89750b2826b687a9e0cb8b90fe5f71cf8a
rezakoushki95 <rezaaarhus@hotmail.com>
2022-09-28 03:40:20 +0200

    pass data between main VC and detail VC

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -14,10 +14,21 @@ class ToDoDetailTableViewController: UITableViewController {
 	@IBOutlet weak var noteView: UITextView!
 	@IBOutlet weak var saveBarButton: UIBarButtonItem!
 	
+	var toDoItem: String!
+	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		nameField.text = toDoItem
+		
+		if toDoItem == nil {
+			toDoItem = ""
+		}
 		
     }
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		toDoItem = nameField.text
+	}
 
 	@IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
 		let isPresentingInAddMode = presentingViewController is UINavigationController
