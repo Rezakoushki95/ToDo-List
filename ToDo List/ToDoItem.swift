15d32668e1984f00c8a21ce6fe699f5e4b901a8d
rezakoushki95 <rezaaarhus@hotmail.com>
2022-10-05 01:10:13 +0200

    - Create a struct for all properties of a ToDoList Item - Modify app to adhere to the new data type

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Reza Koushki on 05/10/2022.
+//
+
+import Foundation
+
+struct ToDoItem {
+	var name: String
+	var date: Date
+	var notes: String
+}
